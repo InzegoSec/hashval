8218bc205651815a1b77cf5afc653af074cd9140
Inzego <inzego@developer.dev>
2026-04-08 21:22:15 -0500

    0804 update

--- hashval.py
+++ hashval.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
-#first commit
+
+import argparse
+import hashlib
+
+algorithms = ["md5", "sha256", "sha1"]
+
+def extract2string(value):
+    return hashlib.md5(value.encode()).hexdigest()
+
+def extract2file(filename):
+    with open(filename, 'rb') as file:
+        digest = hashlib.file_digest(file, 'md5')
+        return digest.hexdigest()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="hashval.py", description="hash tool")
+    args = parser.
